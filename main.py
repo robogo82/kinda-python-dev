9d80e2a8e819fd441c7b1e9f0c4c0ff973cfcdb4
Robert Markoš <robert.markos.hr@gmail.com>
2023-11-15 20:53:11 +0100

    First examples of Flash methods.

--- main.py
+++ main.py
@@ -1,11 +1,31 @@
 from flask import Flask
+from flask.json import jsonify
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello World</p>"
+dict_primjer = {
+    "ime": "Marko",
+    "prezime": "Markić",
+    "email": "marko@markic.hr"
+}
 
-@app.route("/hi")
-def hi_there():
-    return "<p>trolololol</p>"
+@app.route("/")
+def home():
+    return "<h1><a href = '/about'>Home</a></h1>"
+
+@app.route("/about")
+def about():
+    return "<h1>About</h1>"
+
+@app.route("/user/<username>")
+def user(username):
+    return f"<h3>User page for {username}</h3>"
+
+@app.route("/json")
+def json():
+    return jsonify(dict_primjer)
+
+@app.route("/json/<key>")
+def json_value(key):
+    # return dict_primjer[key]
+        return dict_primjer.get(key, "Unknown key")
